ff95b153fc0843d98f346e4d8b9947dc2f052f82
lucasdomzy <136852949+lucasdomzy@users.noreply.github.com>
2024-06-12 21:23:34 -0300

    Update Produto.java

--- Aula24/src/Produto.java
+++ Aula24/src/Produto.java
@@ -1,4 +1,44 @@
 
 public class Produto {
+private String nome;
+	private double valorUnitario;
+	private double quantidade;
+	
+	public Produto() {
+		
+	}
+	
+	public Produto(String nome, double valorUnitario, int quantidade) {
+		this.nome = nome;
+		this.valorUnitario = valorUnitario;
+		this.quantidade = quantidade;
+	}
 
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getValorUnitario() {
+		return valorUnitario;
+	}
+
+	public void setValorUnitario(double valorUnitario) {
+		this.valorUnitario = valorUnitario;
+	}
+
+	public double getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(double quantidade) {
+		this.quantidade = quantidade;
+	}
+	
+	public String toString() {
+		return "Nome: " + nome + "\nValor da unidade: " + valorUnitario + "\nQuantidade em estoque: " + quantidade + "\n";
+	}
 }
